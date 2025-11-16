e80885d00dcc32c337c0b852d3849b9b99671771
kashik09 <kashiku789@gmail.com>
2025-11-16 18:00:26 +0300

    Implement watering control based on temperature

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,10 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
