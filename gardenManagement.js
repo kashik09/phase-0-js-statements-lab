e1fcb1c57e6ef279a3fe3c33d9cd1324a6030dc5
kashik09 <kashiku789@gmail.com>
2025-11-23 13:20:08 +0300

    Fix lights control to use OR operator as expected by tests

--- gardenManagement.js
+++ gardenManagement.js
@@ -2,14 +2,13 @@ const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
 
-
 if (temperature > 80) {
   console.log("Watering on");
 } else {
   console.log("Watering off");
 }
 
-if (["evening", "night"].includes(timeOfDay)) {
+if (timeOfDay === "evening" || timeOfDay === "night") {
   console.log("Lights on");
 } else {
   console.log("Lights off");
